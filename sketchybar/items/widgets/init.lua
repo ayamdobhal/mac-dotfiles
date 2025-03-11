eb87ac4bee1e98090f715497b6fab056e06e39be
Ayam Dobhal <me@iamdobhal.dev>
2025-03-11 20:52:20 +0530

    init

---
+++ sketchybar/items/widgets/init.lua
@@ -0,0 +1,19 @@
+local settings = require("settings")
+local tbl = require("utils.tbl")
+
+local widgets = {
+    "weather",
+    "settings",
+    "clipboard",
+    "battery",
+    "volume",
+    "wifi",
+    "cpu",
+    "ram",
+}
+
+for _, widget in ipairs(widgets) do
+    if tbl.get_index_by_value(settings.hide_widgets, widget) == -1 then
+        require("items.widgets." .. widget)
+    end
+end
